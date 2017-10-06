cfc5df67b499b4ee7498392825e06298428db05f
light <sena.ufc@gmail.com>
2017-10-06 11:36:32 -0300

    fix

--- assets/00_modelo/main.js
+++ assets/00_modelo/main.js
@@ -1,21 +1,41 @@
-var input = require('readline-sync');
-class Operacao {
-    constructor(descricao = "", value = 0.0, saldo = 0.0) {
+/*
+declare function require(name:string);
+var readline = require('readline-sync');
+function cin(text: string): string{
+    return readline.question(text);
+}
+function cout(text: string){
+    return console.log(text);
+}
+*/
+function cin(text) {
+    return prompt(text);
+}
+function cout(text) {
+    return alert(text);
+}
+var Operacao = (function () {
+    function Operacao(descricao, value, saldo) {
+        if (descricao === void 0) { descricao = ""; }
+        if (value === void 0) { value = 0.0; }
+        if (saldo === void 0) { saldo = 0.0; }
         this.value = value;
         this.descricao = descricao;
         this.saldoParcial = saldo;
     }
-    toString() {
+    Operacao.prototype.toString = function () {
         return "[" + this.descricao + ", " + this.value + ", saldo: " + this.saldoParcial + "]";
-    }
-}
-class Conta {
-    constructor(id = 0) {
+    };
+    return Operacao;
+}());
+var Conta = (function () {
+    function Conta(id) {
+        if (id === void 0) { id = 0; }
         this.id = id;
         this.saldo = 0;
         this.extrato = [];
     }
-    sacar(value) {
+    Conta.prototype.sacar = function (value) {
         if (value <= 0)
             return false;
         if (this.saldo > value) {
@@ -24,44 +44,45 @@ class Conta {
             return true;
         }
         return false;
-    }
-    depositar(value) {
+    };
+    Conta.prototype.depositar = function (value) {
         if (value <= 0)
             return false;
         this.saldo += value;
         this.extrato.push(new Operacao("deposito", value, this.saldo));
         return true;
-    }
-    getSaldo() {
+    };
+    Conta.prototype.getSaldo = function () {
         return this.saldo;
-    }
-    getId() {
+    };
+    Conta.prototype.getId = function () {
         return this.id;
-    }
-    getExtrato(qtd) {
-        let last = [];
-        let size = this.extrato.length;
+    };
+    Conta.prototype.getExtrato = function (qtd) {
+        var last = [];
+        var size = this.extrato.length;
         if (!qtd || (qtd > size))
             qtd = size;
-        for (let i = (size - qtd); i < size; i++)
+        for (var i = (size - qtd); i < size; i++)
             last.push(this.extrato[i]);
         return last;
-    }
-}
-class Controller {
-    constructor() {
+    };
+    return Conta;
+}());
+var Controller = (function () {
+    function Controller() {
         this.conta = new Conta(5);
         this.conta.depositar(40);
         this.conta.sacar(5);
         this.conta.sacar(10);
         this.conta.depositar(30);
     }
-    commandLine() {
-        let ui = [""]; //user input
+    Controller.prototype.commandLine = function () {
+        var ui = [""]; //user input
         while (ui[0] != "fim") {
-            ui = input.question("digite comando ou help: ").split(" ");
+            ui = cin("digite comando ou help: ").split(" ");
             if (ui[0] == "help") {
-                console.log("help\n" +
+                cout("help\n" +
                     "iniciar $id\n" +
                     "saldo\n" +
                     "saque $value\n" +
@@ -72,42 +93,47 @@ class Controller {
             }
             if (ui[0] == "iniciar") {
                 this.conta = new Conta(Number(ui[1]));
-                console.log("Conta " + this.conta.getId() + " criada");
+                cout("Conta " + this.conta.getId() + " criada");
             }
             if (ui[0] == "saldo") {
-                console.log("Conta: " + this.conta.getId() + ", Saldo: " + this.conta.getSaldo());
+                cout("Conta: " + this.conta.getId() + ", Saldo: " + this.conta.getSaldo());
             }
             if (ui[0] == "saque") {
                 if (this.conta.sacar(Number(ui[1])))
-                    console.log("Saque efetuado");
+                    cout("Saque efetuado");
                 else
-                    console.log("Erro no saque");
+                    cout("Erro no saque");
             }
             if (ui[0] == "deposito") {
                 if (this.conta.depositar(Number(ui[1])))
-                    console.log("Deposito Efetuado");
+                    cout("Deposito Efetuado");
                 else
-                    console.log("Erro");
+                    cout("Erro");
             }
             if (ui[0] == "extrato") {
-                let output = "Extrato:\n";
+                var output = "Extrato:\n";
                 //of anda nos valores
                 //in nas chaves
-                for (let e of this.conta.getExtrato())
+                for (var _i = 0, _a = this.conta.getExtrato(); _i < _a.length; _i++) {
+                    var e = _a[_i];
                     output += "" + e + "\n";
-                console.log(output);
+                }
+                cout(output);
             }
             if (ui[0] == "extratoLast") {
-                let output = "Extrato:\n";
+                var output = "Extrato:\n";
                 //of anda nos valores
                 //in nas chaves
-                for (let e of this.conta.getExtrato(Number(ui[1])))
+                for (var _b = 0, _c = this.conta.getExtrato(Number(ui[1])); _b < _c.length; _b++) {
+                    var e = _c[_b];
                     output += "" + e + "\n";
-                console.log(output);
+                }
+                cout(output);
             }
         }
-    }
-}
-let controller = new Controller();
+    };
+    return Controller;
+}());
+var controller = new Controller();
 controller.commandLine();
 //# sourceMappingURL=main.js.map
