86fb36dddeebdb88e8f261906f36dce2bd81f3da
David Sena Oliveira <sena.ufc@gmail.com>
2018-08-31 02:12:11 -0300

    calc

--- assets/calculadora/Controller.java
+++ assets/calculadora/Controller.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 class Calculadora {
-	static final int bateriaInicial = 2;
+	static final int bateriaInicial = 0;
 	static final int bateriaMaxima = 5;
 	
 	int bateria;
@@ -16,35 +16,26 @@ class Calculadora {
 		if(this.bateria > Calculadora.bateriaMaxima)
 			this.bateria = Calculadora.bateriaMaxima;
 	}
-	boolean gastarBateria() {
-		if(this.bateria == 0){
-			System.out.print("fail: bateria insuficiente");
-			return false;
-		}
+	void gastarBateria() {
+		if(this.bateria == 0)
+			throw new RuntimeException("failure: bateria insuficiente");
 		this.bateria -= 1;
-		return true;
 	}
 	
-	boolean soma(float a, float b, Float result){
-		if(!gastarBateria())
-			return false;
-		result = a + b;
-		return true;
+	float soma(float a, float b){
+		gastarBateria();
+		return a + b;
 	}
 	
-	boolean divisao(float num, float den, Float result){
-		if(!gastarBateria())
-			return false;
-		if(den == 0f){
-			System.out.print("fail: divisao por zero");
-			return false;
-		}
-		result = num/den;
-		return true;
+	float divisao(float num, float den){
+		gastarBateria();
+		if(den == 0f)
+			throw new RuntimeException("failure: divisao por zero");
+		return num/den;
 	}
 	
 	public String toString(){
-		return "bateria = " + this.bateria;
+		return "battery = " + this.bateria;
 	}
 }
 
@@ -67,31 +58,28 @@ class Controller{
 			saida = "" + calc;
 		else if(ui[0].equals("charge")){
 			calc.charge(Integer.parseInt(ui[1]));
-			saida = "done";
-		}else if(ui[0].equals("soma")){
-			Float result = new Float(0.0);
-			if(calc.soma(Float.parseFloat(ui[1]), Float.parseFloat(ui[2]), result))
-				saida = ": " + result;
+			saida = "success";
+		}else if(ui[0].equals("sum")){
+			saida = "= " + calc.soma(Float.parseFloat(ui[1]), Float.parseFloat(ui[2]));
 		}else if(ui[0].equals("div")){
-			Float result = new Float(0.0);
-			if(calc.divisao(Float.parseFloat(ui[1]), Float.parseFloat(ui[2]), result))
-				saida = "= " + result;
+			saida = "= " + calc.divisao(Float.parseFloat(ui[1]), Float.parseFloat(ui[2]));
 		}else
-			saida = "comando invalido";
+			saida = "failure: comando invalido";
 		return saida;
 	}
 
 	public static void main(String[] args) {
 		Controller cont = new Controller();
-		System.out.println("Digite um comando ou help:");
 		while(true){
 			String line = scan.nextLine();
-			System.out.println(line);
+			System.out.println("$" + line);
 			if(line.equals("end"))
 				break;
-			System.out.print("  ");
-			System.out.println(cont.shell(line));
+			try{
+				System.out.println(cont.shell(line));
+			}catch(RuntimeException e){
+				System.out.println(e.getMessage());
+			}
 		}
 	}
-
 }
--- assets/calculadora/solver.cpp
+++ assets/calculadora/solver.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <sstream>
+#include <iomanip> //setprecision
 using namespace std;
 
 struct Calc{
@@ -7,7 +9,7 @@ struct Calc{
 
     Calc(){
         this->limiteBateria = 5;
-        this->bateria = 2;
+        this->bateria = 0;
     }
     void charge(int carga){
         bateria += carga;
@@ -16,7 +18,7 @@ struct Calc{
     }
     void gastarBateria(){
         if(bateria <= 0)
-            throw "fail: bateria insuficiente";
+            throw "failure: bateria insuficiente";
         this->bateria -= 1;
     }
     float soma(float a, float b){
@@ -26,43 +28,39 @@ struct Calc{
     float div(float a, float b){
         gastarBateria();
         if(b == 0)
-            throw "fail: divisao por zero";
+            throw "failure: divisao por zero";
         return a/b;
     }
 };
 
-#include <sstream>
-template <class T>
-T get(stringstream& ss){
-    T t;
-    ss >> t;
-    return t;
-}
 
 struct Controller{
     Calc calc;
 
     string shell(string line){
         stringstream out;
-        stringstream ui(line);
-        string op = get<string>(ui);
+        stringstream in(line);
+        string op;
+        in >> op;
         if(op == "help"){
             out << "sum _a _b; div _a _b; show; charge _carga; end";
-        }else if(op == "soma"){
+        }else if(op == "sum"){
             float a, b;
-            ui >> a >> b;
-            out << "= " << calc.soma(a, b);
+            in >> a >> b;
+            out << "= " << fixed << setprecision(1) << calc.soma(a, b);
         }else if(op == "div"){
             float a, b;
-            ui >> a >> b;
-            out << "= " << calc.div(a, b);
+            in >> a >> b;
+            out << "= " << fixed << setprecision(1) << calc.div(a, b);
         }else if(op == "charge"){
-            calc.charge(get<int>(ui));
-            out << "done";
+            float charge;
+            in >> charge;
+            calc.charge(charge);
+            out << "success";
         }else if(op == "show"){
-            out << "bateria = " << calc.bateria;
+            out << "battery = " << calc.bateria;
         }else 
-            out << "fail: comando invalido";
+            out << "failure: comando invalido";
         return out.str();
     }
 };
@@ -72,11 +70,11 @@ int main(){
     string line;
     while(true){
         getline(cin, line);
-        cout << line << endl;
+        cout << "$" << line << endl;
         if(line == "end")
             break;
         try{
-            cout << "  " << c.shell(line) << endl;
+            cout << c.shell(line) << endl;
         }catch(const char* error){
             cout << error << endl;
         }
