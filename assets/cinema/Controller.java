d7463c5a5a824a35b0391946f18f532d2cd7c7dc
David Sena <sena.ufc@gmail.com>
2019-05-17 11:00:45 -0300

    agencia com herança

--- assets/cinema/Controller.java
+++ assets/cinema/Controller.java
@@ -1,92 +1,97 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-class Pessoa {
+class Cliente{
     public String id;
     public String fone;
-    
-    public Pessoa(String id, String fone) {
+    public Cliente(String id, String fone){
         this.id = id;
         this.fone = fone;
     }
+
+    @Override
     public String toString() {
-        return "" + this.id + ":" + this.fone;
-    }    
+        return id + ":" + fone;
+    }
 }
 
-class Sala {
-    ArrayList<Pessoa> cadeiras;
-    
-    public Sala(int capacidade) {
-        cadeiras = new ArrayList<Pessoa>();
+class Sala{
+    ArrayList<Cliente> cadeiras;
+
+    public Sala(int capacidade){
+        cadeiras = new ArrayList<Cliente>();
         for(int i = 0; i < capacidade; i++)
             cadeiras.add(null);
     }
-    
-    public void reservar(Pessoa pessoa, int indice) {
-        if(cadeiras.get(indice) != null)
-            throw new RuntimeException("ja esta reservado");
-        cadeiras.set(indice, pessoa);
-    }
-    
-    public void cancelar(String idPessoa) {
-        for(int i = 0; i < cadeiras.size(); i++)
-            if(cadeiras.get(i) != null && cadeiras.get(i).id.equals(idPessoa)) {
-                cadeiras.set(i, null);
-                return;
+
+    boolean reservar(String id, String fone, int ind){
+        if((ind >= this.cadeiras.size()) ||(ind < 0)){
+            System.out.println("fail: indice invalido");
+            return false;
+        }
+        if(this.cadeiras.get(ind) != null){
+            System.out.println("fail: cadeira ja esta ocupada");
+            return false;
+        }
+        for(Cliente cliente : this.cadeiras) {
+            if ((cliente != null) && (cliente.id.equals(id))) {
+                System.out.println("fail: cliente ja esta no cinema");
+                return false;
             }
-        throw new RuntimeException("Pessoa nao encontrada");
+        }
+
+        this.cadeiras.set(ind, new Cliente(id, fone));
+        return true;
     }
 
+    void cancelar(String id){
+        for(int i = 0; i < this.cadeiras.size(); i += 1){
+            Cliente cliente = this.cadeiras.get(i);
+            if((cliente != null) && (cliente.id.equals(id))){
+                this.cadeiras.set(i, null);
+                return;
+            }
+        }
+        System.out.println("fail: cliente nao esta no cinema");
+    }
+
+
+    @Override
     public String toString() {
-        String saida = "";
-        for(Pessoa p : cadeiras) {
-            if(p == null)
+        String saida = "[ ";
+        for (Cliente cliente : cadeiras){
+            if(cliente == null)
                 saida += "- ";
             else
-                saida += p + " ";
+                saida += cliente + " ";
         }
+        saida += "]";
         return saida;
     }
 }
 
-public class Controller{
-    Sala sala;
-    Scanner sca;
-    static final int DEFAULT_CAPACITY = 5;
-    
-    Controller(){
-        sala = new Sala(DEFAULT_CAPACITY);
-        sca = new Scanner(System.in);
-    }
-    
-    public String shell(String line) {
-        String[] ui = line.split(" ");
-        if(ui[0].equals("show"))
-            return "" + sala;
-        else if(ui[0].equals("init"))
-            sala = new Sala(Integer.parseInt(ui[1]));
-        else if(ui[0].equals("reservar"))
-            sala.reservar(new Pessoa(ui[1], ui[2]), 
-                    Integer.parseInt(ui[3]));
-        else if(ui[0].equals("cancelar"))
-            sala.cancelar(ui[1]);
-        return "done";
-    }
-    
-    public void exec() {
-        while(true) {
-            String line = sca.nextLine();
-            try {
-                System.out.println(shell(line));
-            }catch(RuntimeException re){
-                System.out.println(re.getMessage());
+public class Controller {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Sala sala = new Sala(0);
+
+        while(true){
+            String line = scanner.nextLine();
+            System.out.println("$" + line);
+            String[] ui = line.split(" ");
+            if(ui[0].equals("end")){
+                break;
+            }else if(ui[0].equals("init")){
+                sala = new Sala(Integer.parseInt(ui[1]));
+            }else if(ui[0].equals("show")){
+                System.out.println(sala);
+            }else if(ui[0].equals("reservar")){
+                sala.reservar(ui[1], ui[2], Integer.parseInt(ui[3]));
+            }else if(ui[0].equals("cancelar")){
+                sala.cancelar(ui[1]);
+            }else{
+                System.out.println("fail: comando invalido");
             }
         }
-    }
-    
-    public static void main(String[] args) {
-        Controller c = new Controller();
-        c.exec();
     }
 }
